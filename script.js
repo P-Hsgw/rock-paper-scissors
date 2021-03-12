10b7f20e08778ad5f7e361eee059bbbaffd08c7c
P-Hsgw <contact@p-hsgw.com>
2021-03-12 11:19:13 +0900

    Updated the game mechanism. Added results logged into a console.

--- script.js
+++ script.js
@@ -3,26 +3,33 @@ const PAPER = "paper";
 const SCISSORS = "scissors";
 
 
-function computerPlay () {
-    let choice = Math.floor(Math.random()*3)
-    if (choice === 0) {
-        return ROCK;
-    } else if (choice === 1) {
-        return SCISSORS;
-    } else if (choice === 2) {
-        return PAPER;
+    function computerPlay () {
+        let choice = Math.floor(Math.random()*3)
+        if (choice === 0) {
+            return ROCK;
+        } else if (choice === 1) {
+            return SCISSORS;
+        } else if (choice === 2) {
+            return PAPER;
+        }
     }
-}
 
-let player_choice = prompt("Enter your choice.");
-let computer_choice = computerPlay();
+    let player_choice = prompt("Enter your choice.");
+    let computer_choice = computerPlay();
 
-function game () {
-    
-}
+    function round (computer_choice, player_choice) {
 
-
+        if (computer_choice == player_choice) {
+            return `Computer chose ${computer_choice}. It's a tie!`; 
+        } else if (computer_choice == "scissors" && player_choice == "rock" || computer_choice == "rock" && player_choice == "paper" || computer_choice == "paper" && player_choice == "scissors") {
+            return `Computer chose ${computer_choice}. You got a point!` ;
+        } else {
+            return `Computer chose ${computer_choice}. You lost a point!`;
+        }
+    }
+    console.log(round(computer_choice, player_choice))
 /*
+
 function humanPlay () {
     if (player_choice.toLocaleLowerCase() == "scissors") {
         return SCISSORS;

af24ac64d4a463e5055685c1125cce0ff1ff424e
P-Hsgw <contact@p-hsgw.com>
2021-03-12 16:06:39 +0900

    Added main functionalities - the game is working correctly. Game loop will last until player or computer reaches 5 points. Small bugs still need fixing.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Basic variables
 let player_score = 0
 let computer_score = 0
 
+// Main game function
 function game () {
-    function computerPlay () {
+    // Random computer choice
+    function computerPlay () { 
         let choice = Math.floor(Math.random()*3)
     
         if (choice === 0) {
@@ -13,45 +16,58 @@ function game () {
             return "scissors"
         }
     };
-
+    
+    // Variables with computer and player choices
     let computer_selection = computerPlay()
     let player_selection = prompt("Enter your choice - rock, paper or scissors:").toLowerCase();
 
+    // Single round function - returning game outcome each round
     function singleRound (computer_selection, player_selection) {
         if (computer_selection === player_selection) {
             return `Computer chose ${computer_selection}. It's a tie.`
-        } else if (computer_selection == "scissors" && player_selection == "paper" || computer_selection == "rock" && player_selection == "scissors" || computer_selection == "paper" && player_selection == "rock") {
+        } else if (computer_selection == "scissors" && player_selection == "paper" 
+        || computer_selection == "rock" && player_selection == "scissors" 
+        || computer_selection == "paper" && player_selection == "rock") {
             return `Computer chose ${computer_selection}. You lose a point.`
         } else {
             return `Computer chose ${computer_selection}. You got a point.`
         }
-    }
+    };
 
+    // Simple score counter, incrementing scores for single round winner
     function scoreCounter (computer_selection, player_selection) {
         if (computer_selection === player_selection) {
             ;
         } else if (computer_selection == "scissors" && player_selection == "paper" 
         || computer_selection == "rock" && player_selection == "scissors" 
         || computer_selection == "paper" && player_selection == "rock") {
-            return computer_score++
+            return computer_score++;
         } else {
-            return player_score++
+            return player_score++;
         }
         
-    }
+    };
 
    console.log(singleRound(computer_selection, player_selection))
    scoreCounter(computer_selection, player_selection)
-   console.log(`Player score ${player_score}; Computer score ${computer_score}`)
+   
+};
 
-}
+// Basic game loop - looping until one of the players reaches 5 points
+function gameLoop () {
+    while (player_score <= 4 && computer_score <= 4) {
+        game()
+        console.log(`Player score: ${player_score}; Computer score: ${computer_score}`)
 
-function gameLoop (player_score) {
-    do {
-        game ();
-    }
-    while (player_score > 5)
+        if (player_score == 5) {
+            return console.log('Player won');
+        } else if (computer_score == 5) {
+            return console.log(`Computer won`);
+        } else {
+            ;
+        }
+    } 
+    
+};
 
-}
-
-gameLoop(player_score);
+gameLoop();

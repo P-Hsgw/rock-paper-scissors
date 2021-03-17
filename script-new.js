44378bfa43afef2d134cb64a9ed29bc76ca51d94
P-Hsgw <contact@p-hsgw.com>
2021-03-17 12:23:19 +0900

    Finish basic game loop including player choice

--- script-new.js
+++ script-new.js
@@ -1,10 +1,13 @@
 // Basic variables
 let player_score = 0
 let computer_score = 0
-
-
-
-// Main game function
+const out = document.querySelector('.output');
+const outcome = document.querySelector('.outcome');
+const btns = document.querySelectorAll(".btn");
+const p = document.querySelector(".player_scoring")
+const c = document.querySelector(".computer_scoring")
+let playerChoice = ""
+let computerChoice = ""
 
 // Random computer choice
 function computerPlay () { 
@@ -19,61 +22,49 @@ function computerPlay () {
   }
 };
 
-
-
-const out = document.querySelector('.result');
-const btns = document.querySelectorAll(".btn");
-
-
-let playerChoice = ""
-
-let computerChoice = ""
-
-
+// Single round logic
 function singleRound () {
   if (playerChoice == computerChoice ) {
-    console.log ("It's a tie!")
+    out.innerHTML = `Computer chose ${computerChoice}. It's a tie!`
   } else if (computerChoice == "scissors" && playerChoice == "paper" 
   || computerChoice == "rock" && playerChoice == "scissors" 
   || computerChoice == "paper" && playerChoice == "rock") {
-      console.log("It's a loss")
+      out.innerHTML = `Computer chose ${computerChoice}. You lost a point!`
+      computer_score++
   } else {
-      console.log("It's a win!")
+      out.innerHTML = `Computer chose ${computerChoice}. You got a point!`
+      player_score++
+      return;
   }
 
+};
+
+// End of game logic - what happens after score = 3
+function endOfGame () {
+  if (player_score == 3) {
+    outcome.innerHTML = "You win! Wait for page to refresh."
+    setTimeout(function(){ location.reload(); }, 2000);
+
+    return;
+  } else if (computer_score == 3) {
+    outcome.innerHTML = "You lose! Wait for page to refresh."
+    setTimeout(function(){ location.reload(); }, 2000);
+
+    return;
+  } else {
+    return ;
+  }
 }
 
+// Game logic, includes player choice and calls other functions
 for (const btn of btns) {
   btn.addEventListener('click', e => {
     playerChoice = e.target.dataset.choice;
     console.log(playerChoice)
     computerChoice = computerPlay();
-    console.log(computerChoice)
     singleRound();
+    p.innerHTML = player_score
+    c.innerHTML = computer_score
+    endOfGame ()
   });
-
-  
 }
-
-
-
-/*
-
-    // Simple score counter, incrementing scores for single round winner
-    function scoreCounter (computer_selection, player_selection) {
-        if (computer_selection === player_selection) {
-            ;
-        } else if (computer_selection == "scissors" && player_selection == "paper" 
-        || computer_selection == "rock" && player_selection == "scissors" 
-        || computer_selection == "paper" && player_selection == "rock") {
-            return computer_score++;
-        } else {
-            return player_score++;
-        }
-        
-    };
-/*
-   console.log(singleRound(computer_selection, player_selection))
-   scoreCounter(computer_selection, player_selection)
-   
-*/

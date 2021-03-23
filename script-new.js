f696e7c5ac6154a0f231cc172c3100ed956960e3
P-Hsgw <contact@p-hsgw.com>
2021-03-23 15:32:15 +0900

    Fix a bug where player could play even when game has ended

--- script-new.js
+++ script-new.js
@@ -1,13 +1,14 @@
 // Basic variables
 let player_score = 0
 let computer_score = 0
-const out = document.querySelector('.output');
-const outcome = document.querySelector('.outcome');
-const btns = document.querySelectorAll(".btn");
-const p = document.querySelector(".player_scoring")
-const c = document.querySelector(".computer_scoring")
+const out = document.querySelector('.output'); // Result of a round
+const outcome = document.querySelector('.outcome'); // Result of a game
+const btns = document.querySelectorAll(".btn"); 
+const p = document.querySelector(".player_scoring") // Player score span
+const c = document.querySelector(".computer_scoring") // Computer score span
 let playerChoice = ""
 let computerChoice = ""
+let gameEnded = false;
 
 // Random computer choice
 function computerPlay () { 
@@ -24,17 +25,19 @@ function computerPlay () {
 
 // Single round logic
 function singleRound () {
-  if (playerChoice == computerChoice ) {
+  if (playerChoice == computerChoice && gameEnded === false) {
     out.innerHTML = `Computer chose ${computerChoice}. It's a tie!`
-  } else if (computerChoice == "scissors" && playerChoice == "paper" 
-  || computerChoice == "rock" && playerChoice == "scissors" 
-  || computerChoice == "paper" && playerChoice == "rock") {
+  } else if (computerChoice == "scissors" && playerChoice == "paper" && gameEnded === false
+  || computerChoice == "rock" && playerChoice == "scissors" && gameEnded === false
+  || computerChoice == "paper" && playerChoice == "rock" && gameEnded === false) {
       out.innerHTML = `Computer chose ${computerChoice}. You lost a point!`
       computer_score++
-  } else {
+  } else if (gameEnded === false){
       out.innerHTML = `Computer chose ${computerChoice}. You got a point!`
       player_score++
       return;
+  } else {
+    return;
   }
 
 };
@@ -42,11 +45,13 @@ function singleRound () {
 // End of game logic - what happens after score = 3
 function endOfGame () {
   if (player_score == 3) {
+    gameEnded = true;
     outcome.innerHTML = "You win! Wait for page to refresh."
     setTimeout(function(){ location.reload(); }, 2000);
 
     return;
   } else if (computer_score == 3) {
+    gameEnded = true;
     outcome.innerHTML = "You lose! Wait for page to refresh."
     setTimeout(function(){ location.reload(); }, 2000);
 
@@ -58,9 +63,9 @@ function endOfGame () {
 
 // Game logic, includes player choice and calls other functions
 for (const btn of btns) {
+
   btn.addEventListener('click', e => {
     playerChoice = e.target.dataset.choice;
-    console.log(playerChoice)
     computerChoice = computerPlay();
     singleRound();
     p.innerHTML = player_score
